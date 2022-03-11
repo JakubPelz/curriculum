ff00506fffc2ef177e029628b690c3534a8633f0
Jakub Pelz <JakubMac@Jakub-MacBook-Air.local>
2022-03-11 19:03:17 +0100

    3rd push

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="ui inverted verical masthead center aligned segment">
+      <div className="ui text container" style={{ padding: '100px' }}>
+        <h1 className="ui inverted header">Insight into my skills</h1>
+        <h3>developed in MERN stack</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
